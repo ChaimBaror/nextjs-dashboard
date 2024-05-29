d14131f6c4a8684341fe67054638678dd3904122
Chaim Bar-or <chaimb@ravtech.local>
2024-05-29 17:36:39 +0300

    add page customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <div>Customers</div>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const customers = await fetchFilteredCustomers(searchParams?.query || '');
+
+  return <CustomersTable customers={customers} />;
 }
